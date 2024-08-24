38a6bbf1bd6dc9c3876be9f183043f7224f5ae6e
Bhargavi <161690421+Bhargavi-Panuganti@users.noreply.github.com>
2024-08-24 14:55:07 +0530

    Update prac.py

--- prac.py
+++ prac.py
@@ -2,6 +2,6 @@ import streamlit as st
 import graphviz as graphviz
 st.write("Displaying an Images")
 # Displaying Image by specifying path
-st.image("files/animal7.jpg")
+st.image("fav.png")
 #Image Courtesy by unplash
 st.write("Image Courtesy: unplash.com")
