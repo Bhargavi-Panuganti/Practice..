5cc1c20dbecd0b8d3844038b83f5de6d1455176d
Bhargavi <161690421+Bhargavi-Panuganti@users.noreply.github.com>
2024-08-24 14:22:42 +0530

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,6 +1,12 @@
 import streamlit as st
-import pandas as pd
-st.write(pd.DataFrame({
-'column one': [5.436, 6.372, 3.645, 4.554, 7.263],
-'column two': [99, 55, 75, 41, 37],
-}))
+import graphviz as graphviz
+st.title('Graphviz')
+# Creating graph object
+st.graphviz_chart('''
+digraph {
+"Training Data" -> "ML Algorithm"
+"ML Algorithm" -> "Model"
+"Model" -> "Result Forecasting"
+"New Data" -> "Model"
+}
+''')
