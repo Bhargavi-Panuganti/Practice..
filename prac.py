d2abc04a7e229c6b22c45513c1486aff23959a20
Bhargavi <161690421+Bhargavi-Panuganti@users.noreply.github.com>
2024-08-24 15:06:34 +0530

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,7 +1,22 @@
 import streamlit as st
-import graphviz as graphviz
-st.write("Displaying an Images")
-# Displaying Image by specifying path
-st.image("fav.png")
-#Image Courtesy by unplash
-st.write("Image Courtesy: unplash.com")
+import base64
+# Function to set Image as Background
+def add_local_backgound_image_(image):
+with open(image, "rb") as image:
+encoded_string = base64.b64encode(image.read())
+st.write("Image Courtesy: unplash")
+st.markdown(
+f"""
+<style>
+.stApp {{
+background-image: url(data:fav
+{"png"};base64,{encoded_string.decode()});
+background-size: cover
+}}
+</style>
+""",
+unsafe_allow_html=True
+)
+st.write("Background Image")
+# Calling Image in function
+add_local_backgound_image_('fav.png')
